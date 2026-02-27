3198d045b794c4b29a17088ce506f85ab29be9e0
김우영 <wooyoung.kim365@gmail.com>
2026-02-27 02:01:58 -0800

    feat: add parallel QuickSort
    Signed-off-by: 김우영 <wooyoung.kim365@gmail.com>

--- Makefile
+++ Makefile
@@ -17,5 +17,14 @@ p2a: p2a.c
 p2b: p2b.c
 	gcc -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -L/opt/homebrew/opt/libomp/lib -lomp -o p2b p2b.c
 
+p3: p3.c
+	gcc -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -L/opt/homebrew/opt/libomp/lib -lomp -o p3 p3.c
+
+p3a: p3a.c
+	gcc -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -L/opt/homebrew/opt/libomp/lib -lomp -o p3a p3a.c
+
+p3b: p3b.c
+	gcc -Xpreprocessor -fopenmp -I/opt/homebrew/opt/libomp/include -L/opt/homebrew/opt/libomp/lib -lomp -o p3b p3b.c
+
 clean:
 	rm -f p1a p1b p1c p2a p2b
---
+++ p3.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+#define size 2*1024*1024
+
+void quickSort(int *array, int start, int end) {
+    if (start >= end) return;
+    int pivot = array[start + (end - start) / 2];
+    int i = start;
+    int j = end;
+    
+    while (i <= j) {
+        while (array[i] < pivot) i++;
+        while (array[j] > pivot) j--;
+        if (i <= j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }    
+    if (start < j) quickSort(array, start, j);
+    if (i < end) quickSort(array, i, end);
+}
+
+int main(void) {
+    int i;
+    struct timespec start, stop; 
+    double exe_time;
+    srand(time(NULL)); 
+    
+    int *m = (int *) malloc(sizeof(int) * size);
+    for(i = 0; i < size; i++) {
+        //m[i] = size - i;
+        m[i] = rand();
+    }
+    
+    if (clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime"); }
+    int pivot_idx = rand() % size;
+    int pivot = m[pivot_idx];
+    int l = 0;
+    int r = size - 1;
+    
+    while (l <= r) {
+        while (m[l] < pivot) l++;
+        while (m[r] > pivot) r--;
+        if (l <= r) {
+            int temp = m[l];
+            m[l] = m[r];
+            m[r] = temp;
+            l++;
+            r--;
+        }
+    }    
+    int f = l;
+    #pragma omp parallel sections num_threads(2)
+    {
+        #pragma omp section
+        {
+            if (f - 1 >= 0) quickSort(m, 0, f - 1);
+        }
+        #pragma omp section
+        {
+            if (f < size) quickSort(m, f, size - 1);
+        }
+    }
+    
+    if (clock_gettime(CLOCK_REALTIME, &stop) == -1) { perror("clock gettime"); }       
+    exe_time = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / 1e9;
+    
+    for(i = 0; i < 16; i++) printf("%d ", m[i]);      
+    printf("\nExecution time = %f sec\n",  exe_time);		
+
+    free(m);
+    return 0;
+}
